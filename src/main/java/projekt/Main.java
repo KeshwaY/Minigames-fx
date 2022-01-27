6a703dcf9c5dd3f210f94fa53e56de20d37348dc
Radek Grędel <radoslaw.gredel@student.pk.edu.pl>
2022-01-27 17:11:45 +0100

    init fx

--- src/main/java/projekt/Main.java
+++ src/main/java/projekt/Main.java
@@ -1,5 +1,6 @@
 package projekt;
 
+import projekt.GUI.MainFX;
 import projekt.client.Client;
 import projekt.server.core.ServerImpl;
 import projekt.server.core.ThreadManagerImpl;
@@ -13,30 +14,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AtomicInteger posOfStartFlag = new AtomicInteger(-1);
-        IntStream.range(0, args.length).boxed()
-                .forEach(i -> {
-                    if (args[i].equals("-start")) {
-                        posOfStartFlag.set(i);
-                    }
-                });
-        String startType = posOfStartFlag.get() >= 0 ? args[posOfStartFlag.get() + 1].toLowerCase(Locale.ROOT) : "client";
-        if (startType.equals("server")) {
-            try {
-                ThreadManagerImpl threadManager = new ThreadManagerImpl(1);
-                ServerImpl serverImpl = new ServerImpl(8080, threadManager);
-                serverImpl.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                Client client = new Client("localhost", 8080);
-                client.test();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        //launches GUI
+        MainFX.main(args);
+
+//        AtomicInteger posOfStartFlag = new AtomicInteger(-1);
+//        IntStream.range(0, args.length).boxed()
+//                .forEach(i -> {
+//                    if (args[i].equals("-start")) {
+//                        posOfStartFlag.set(i);
+//                    }
+//                });
+//        String startType = posOfStartFlag.get() >= 0 ? args[posOfStartFlag.get() + 1].toLowerCase(Locale.ROOT) : "client";
+//        if (startType.equals("server")) {
+//            try {
+//                ThreadManagerImpl threadManager = new ThreadManagerImpl(1);
+//                ServerImpl serverImpl = new ServerImpl(8080, threadManager);
+//                serverImpl.start();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        } else {
+//            try {
+//                Client client = new Client("localhost", 8080);
+//                client.test();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
 }
